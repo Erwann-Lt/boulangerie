b134482d1eb648d36bdd026767acc4d0c94b1df2
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2021-11-24 10:53:23 +0100

    Update Intro.js

--- src/patisserie/Intro.js
+++ src/patisserie/Intro.js
@@ -6,10 +6,10 @@ const Intro = () => {
            <h2>PÂTISSERIES ARTISANALES <br/>
            100% FAITES MAISON</h2>
             <hr/> 
-            <p>À la boulangerie du Er, nous vous proposons <br/>
+            <p>À la boulangerie d'Ymare, nous vous proposons <br/>
             un vaste choix de pâtisseries, toutes réalisées maison.<br/>
             <br/>
-            Le Er, pâtissier de formation, vous présente des <br/>
+            Notre boulanger, pâtissier de formation, vous présente des <br/>
             versions
             revisitées de certains classiques sucrés, ainsi <br/>
             que des gâteaux sur mesure.
