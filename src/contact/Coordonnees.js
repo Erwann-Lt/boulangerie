9c1bf29b3a201d45e6dfa667554bf97287908d66
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2021-11-30 15:04:24 +0100

    Update Coordonnees.js

--- src/contact/Coordonnees.js
+++ src/contact/Coordonnees.js
@@ -1,15 +1,32 @@
 import React from 'react'
+import useWindowSize from '../useWindowSize'
+
 
 const Coordonnees = () => {
+    const { width } = useWindowSize();
+
     return (
         <div className='coordonnees'>
-            <h4>BOULANGERIE D'YMARE</h4>
+
+        {width <650 ?
+        
+        <div>
+        <h4>BOULANGERIE-PÂTISSERIE D'YMARE</h4>
             <p>464 grande rue</p>
             <p> 76520 YMARE</p>
              
             <h5>Téléphone</h5>
-            <p>02 27 76 24 70</p>
+            <p>02 27 76 24 70</p>    
+        </div>
+        
+        :
+        <div>
+        <h4>BOULANGERIE-PÂTISSERIE DU 'Ymare'</h4>
+            <p>464 grande rue</p>
+            <p> 76520 YMARE</p>
              
+            <h5>Téléphone</h5>
+            <p>02 27 76 24 70</p>    
             <h5>Horaires</h5>
             <h6>Du mardi au vendredi :</h6>
             <p>06h30-13h00 et 15h00-19h30</p>
@@ -22,6 +39,13 @@ const Coordonnees = () => {
 
             <img src='https://www.boulangerie-ymare.fr/wp-content/uploads/2019/10/boulangerie_intro.jpg'
             alt='coordopic'className='coordopic' />
+        </div>
+
+
+        }
+            
+             
+     
         </div>
     )
 }
