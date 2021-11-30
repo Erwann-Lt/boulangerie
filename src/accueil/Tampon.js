31e6569131bcfdf4cdea2b355698e6b2eaba46dc
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2021-11-30 14:50:30 +0100

    Update Tampon.js

--- src/accueil/Tampon.js
+++ src/accueil/Tampon.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import useWindowSize from '../useWindowSize'
 
+ 
 const Tampon = () => {
+    const { width } = useWindowSize();
+
     return (
+        <div>
+        {width <650 ? 
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg"
+         width="60" height="60" fill="red" class="bi bi-bookmark-check" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+        </svg>
+        <h4 style={{fontSize:'10px'}}>Des matières premières locales <br/> et de qualité</h4>
+        </div>
+        :
         <div>
             <svg xmlns="http://www.w3.org/2000/svg"
          width="120" height="120" fill="red" class="bi bi-bookmark-check" viewBox="0 0 16 16">
@@ -9,6 +24,9 @@ const Tampon = () => {
         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
         </svg>
         <h4>Des matières premières locales et de qualité</h4>
+        </div>
+        }
+             
         </div>
     )
 }
