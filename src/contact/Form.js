8dd6b13d1f47048f780e925e9a96535807413585
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2021-11-30 15:13:50 +0100

    Update Form.js

--- src/contact/Form.js
+++ src/contact/Form.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import useWindowSize from '../useWindowSize'
 
 const Form = () => {
-    
+         const { width } = useWindowSize();
+
     return (
         <form >
-            <input type="text" placeholder='Nom*' />
+        {width < 650 ?
+            <div>
+              <input type="text" placeholder='Nom*' />
+            <br/> <br/>
+            <input   type="text" placeholder='Prénom*' />
+            <br/> <br/>
+            <input type="text" placeholder='Email*' />
+            <br/> <br/>
+            <textarea type="text" placeholder='Message*' rows='5' cols='21'/>
+            <br/> <br/>   
+            </div>
+
+            :
+
+            <div>
+             <input type="text" placeholder='Nom*' />
             <br/> <br/>
             <input type="text" placeholder='Prénom*' />
             <br/> <br/>
@@ -15,7 +32,11 @@ const Form = () => {
             <input type="text" placeholder='Objet*' />
             <br/> <br/>
             <textarea type="text" placeholder='Message*' rows='10' cols='41'/>
-            <br/> <br/>
+            <br/> <br/>   
+            </div>
+           
+        }
+            
              
             <button className='btn btn-outline-dark'>Envoyer</button>
         </form>
